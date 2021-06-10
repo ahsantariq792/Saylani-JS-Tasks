17d0a35dcbc7eb3223eb357da0c1b805488b5c92
Ahsan Tariq <61455261+ahsantariq792@users.noreply.github.com>
2021-06-11 03:25:02 +0500

    Chap 1 to 5
    JS Tasks chap 1-5

---
+++ Chap 1-5/App.js
@@ -0,0 +1,217 @@
+//  Chapter #1 Solutions :
+
+
+// Question # 1
+// var a="Welcome to my Website"
+// alert(a);
+
+
+// Question # 2
+// var b= "Error! Please enter a valid password"
+// alert(b);   
+
+ 
+// Question # 3
+// var c = "Welcome to JS land..."
+// var d ="Happy Coding!"   
+// alert(c )
+// alert(d)
+
+
+// Question # 4
+// var c = "Welcome to JS land"
+// var d ="Prevent this page from craeting additional dialogs"
+// alert(c )
+// alert(d)
+
+ 
+// Question # 5
+// var x ="Hello... I can run JS through my browser's console"            
+// console.log(x)
+
+
+
+//________________________________________________________________________________________________
+
+// Chapter #2 Solutions:
+
+
+
+
+// Question # 1
+// var s = username;
+
+
+// Question # 2
+// myname= Ahsan;   
+
+
+// Question # 3
+// message = "Hello world"
+// alert(message);
+
+// Question # 4
+// a=prompt("Enter Name")
+// b=prompt("Enter age")
+// c=prompt("Enter Certificate")
+// alert(a)
+// alert(b)
+// alert(c)
+
+// Question # 5
+// var a = "PIZZA"; 
+// var b= "PIZZ";
+// var c= "PIZ";
+// var d= "PI";
+// var e= "P";               
+// alert(a + "\n" + b +"\n" + c+"\n" + d+"\n" + e);
+
+// Question # 6
+// var a = "ahsantariq@gmail.com" 
+// alert("My email address is :" + a )  
+
+
+// Question # 7
+//   var book = "A smarter way to learn javascript" 
+// alert("I'm trying to learning from the book :" + book ) 
+
+
+// Question # 8
+//  var x = "Yah! I can write HTML content through javascript"
+//  document.write(x);
+
+//// Question # 9
+// var w ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"                
+// alert(w)
+
+
+//________________________________________________________________________________________________
+
+
+// Chapter #3 Solutions :
+
+
+// Question # 1
+// var age=18;
+// alert("My age is :" + age) 
+
+
+
+// Question # 3
+// var birthyear= 2001;
+// document.write("My birth year is :"+ birthyear)
+
+
+// Question # 4
+// var a= prompt("Please enter your name here  ");
+// var b = prompt("What do you want to buy from our store?")
+// var c = prompt("How many" + " " + b + " " +  "do you want to buy from our store?")
+// var d = "Buy Time SuperMarket"
+// document.write(a + "ordered"+ c + b + "from" + d)
+ 
+//________________________________________________________________________________________________
+
+// Chapter #5 Solutions:
+
+
+
+// Question # 4
+//  var ticket = 600;
+//  x = 600 * 5;
+//  document.write("Total cost to buy 5 tickets to a movie is :" + x + "PKR")
+
+
+
+// Question # 5
+// var n = +prompt("Enter the number");
+// for(var a=1; a<=10; a++)
+// {
+//     document.write( n + "*" + a + "=" + n*a + "<br>")
+// }
+
+ 
+// Question # 6
+// var t=25;
+// var F =(t *9 /5 ) +32
+// document.write(t + "is" + F + "<br>");
+// var s= 77;
+// var C = (s -32 )* 5/9;
+// document.write(s + "is" + C );
+
+ 
+// Question # 7
+// var a = "Price of item 1";
+// var a1 = +prompt("Enter price of item 1");
+// var b = "Ordered quantity of item 1";
+// var b1 = +prompt("Enter quantity of item 1");
+// var c = "Price of item 2";
+// var c1 = +prompt("Enter price of item 2");
+// var d = "Ordered quantity of item 2";
+// var d1=  +prompt("Enter quantity of item 2");
+// var ShippingCharges = 100;
+// document.getElementById('h1');
+// document.write( a + "is : " + a1 + "<br>")
+// document.write( b +" is :" + b1 + "<br>")
+// document.write( c +"is : " + c1 + "<br>")
+// document.write( d +"is :" + d1 + "<br>")
+// document.write("Shipping Charges :" + ShippingCharges + "<br>" +"<br>" +"<br>")
+// var total = ( x=a1*b1) + (y=c1*d1);
+// document.write("Total cost of Your order is :" + total + "<br>")
+
+ 
+
+// Question # 8
+// var max = 100;
+// var min = 40;
+// var p= (min*100)/max;
+// document.getElementById('h8')
+// document.write("Maximum marks :" + max + "<br>")
+// document.write("Minimum marks :" + min + "<br>")                
+// document.write("Percentage :" + p + "<br>")
+
+ 
+// Question # 9
+// var USD= 10;
+// var SR= 25;
+// var PKR= USD*140;
+// var pkr=  SR * 28;
+// document.getElementById('h9');
+// document.write("Total Currency in PKR from US Dollars is :" + PKR + "<br>")
+// document.write("Total Currency in PKR from Saudi Riyal is :" + pkr + "<br>")
+                // Question # 10
+// var a = 10 ; a += 5 ; a *= 10 ; a/=2;
+// document.write(a);
+
+
+// Question # 11
+// var a = +prompt("Current Year");
+// var b = +prompt("Birth Year");
+// var c = 
+// document.getElementById('h11')
+// document.write("Current Year :" + a + "<br>")
+// document.write("Birth Year :" + b + "<br>")
+// document.write("Your age is :" + c + "<br>")
+
+
+// Question # 12
+// var r = 2;                
+// var c = 2 * 3.142 * r ;
+// var a = 3.142 * r * 2;
+// document.getElementById('h12');
+// document.write("Radius of circle is :" + r + "<br>")
+// document.write("The Circumference is :" + c + "<br>")
+// document.write("The Area is :" + a + "<br>")
+
+
+// Question # 13
+// var snack = "Lays";
+// var age = 18;
+// var a = 64;
+// var b = 2;
+// var c = a-age*b;
+// document.getElementById('h13')
+// document.write("Favourite Snack : " + snack + "<br>")
+// document.write("Current Age : " + age + "<br>")
+// document.write("Maximum Age : " + a + "<br>")
+// document.write("Amount of snack per day : " + b + "<br>" + "<br>")
+// document.write("You will need " + " " + c + " " + snack + " " + "to last you untill the ripe old age of " + a  )
