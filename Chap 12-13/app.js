65e0030c5cb335fd0ed9b317157394e9631e0fb3
Ahsan Tariq <61455261+ahsantariq792@users.noreply.github.com>
2021-06-11 03:27:05 +0500

    chap 12 to 13
    JS Tasks chap 12 to 13

---
+++ Chap 12-13/app.js
@@ -0,0 +1,75 @@
+// Chapter 12 to 13 Solutions :
+
+
+// Question 1
+// var A=+prompt("ENTER NUMBER") 
+// if(A<0){
+//     alert("Number is Negative")
+// }
+// else if(A>0){
+//     alert("Number is Positive")
+// }
+// else{
+//     alert("Number is Zero")
+// }
+
+
+
+// Question 2
+// var A=+prompt("ENTER NUMBER")
+// var B=+prompt("ENTER NUMBER")
+// if(A>B){
+//     alert(A + " is larger")
+// }
+// else if(A<B){
+//     alert(B + " is larger")
+// }
+// else
+// {
+//     alert("Number is Equal")
+// }
+
+
+
+// Question 4
+// var vovel=["A","a","E","e","I","i","O","o","U","u"]
+// var A=prompt("ENTER CHARACTER")
+// if(A==vovel){
+//     alert("True Character is Vovel")
+// }
+// else{
+//     alert("False  Character is Not Vovel")
+// }
+
+
+
+// Question 5
+// var enter=prompt("ENTER PASSWORD")
+// var pass="BASIT"
+// if(enter == " ")
+// {
+//     alert(" “ Please enter your password” ")
+// }
+// else if(enter == pass)
+// {
+//     alert(" “Correct! The password you entered matches the original password” ")
+// }
+// else
+// {
+//     alert(" “Incorrect password” ")
+// }
+
+
+
+// Question 6
+// var greeting;
+// var hour = 13;
+// if (hour < 18)
+// {
+// document.write("Good day")
+// }
+// else
+// {
+// document.write("Good evening")
+// }
+
